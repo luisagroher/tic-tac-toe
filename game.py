8eb35a40fd8c47335b86ba8c50d15743a972186b
luisa groher <luisagroher@pop-os.localdomain>
2024-06-26 09:15:04 -0400

    added functions to create_game, check available positions, and make move

--- game.py
+++ game.py
@@ -1,4 +1,24 @@
+import numpy as np
+import pandas as pd
+
+
 # board representation
+
+def create_board():
+    return np.array(['__'] * 9).reshape(3, 3)
+
+def show_board(board):
+    return pd.DataFrame(board)
+
+def create_game():
+    board = create_board()
+    positions = available_positions(board)
+    return {
+        'board': board,
+        'current_player': 'X',
+        'result': None,
+        'positions': positions,
+    }
 
 # check for win
 
@@ -6,7 +26,20 @@
 
 # get possible moves / actions
 
+def available_positions(board):
+    x, y = np.where(board == '__')
+    return [(x, y) for x, y in zip(x, y)]
+
 # make a move
+
+def make_move(game, position):
+    board = game['board']
+    current_player = game['current_player']
+    board[position] = current_player
+    game['board'] = board
+    game['current_player'] = 'X' if current_player == 'O' else 'O'
+    game['positions'] = available_positions(board)
+    return game
 
 # minimax function
 
